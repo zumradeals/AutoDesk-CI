60c6dfd85a1ddbae70e8fcd69d31465537298245
zumradeals <zumradeals@gmail.com>
2026-07-26 18:24:28 +0000

    feat: redesign src/components/layout/Layout.tsx

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,19 +1,20 @@
-import { Outlet, useLocation } from 'react-router-dom';
 import { useEffect } from 'react';
+import { Outlet, useLocation } from 'react-router-dom';
 import { Header } from './Header';
 import { Footer } from './Footer';
 import { WhatsAppFloating } from './WhatsAppFloating';
 
 export function Layout() {
   const { pathname } = useLocation();
+
   useEffect(() => {
     window.scrollTo({ top: 0, behavior: 'instant' });
   }, [pathname]);
 
   return (
-    <div className="min-h-screen flex flex-col bg-white">
+    <div className="flex min-h-screen flex-col bg-white">
       <Header />
-      <main className="flex-1 pt-16">
+      <main className="flex-1 pt-16 sm:pt-24 lg:pt-[104px]">
         <Outlet />
       </main>
       <Footer />
