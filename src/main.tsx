a0190101f4b6ec2e1bc03a8e71bcc0cfa1113dba
zumradeals <zumradeals@gmail.com>
2026-07-26 19:01:45 +0000

    initialize configurable Meta Pixel

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,9 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import { initMetaPixel } from './utils/metaPixel';
+
+initMetaPixel();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
